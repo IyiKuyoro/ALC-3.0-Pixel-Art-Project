2b5c79a79c6fa9d19dadebead37b1ab72accf400
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-06-06 15:12:42 +0100

    Correct error with changing cell color

--- designs.js
+++ designs.js
@@ -74,14 +74,31 @@ $('table').on('click', 'td',function(){
     var color = $('#colorPicker').val();
     var cell = $(this);
 
-    if(cell.attr("class") === "painted"){
+    if(cell.attr("class") === "painted" && (colorToHex(cell.css("background-color")) === color)){
         cell.css("background-color", "white");
     }else{
         cell.css("background-color", color);
     }
 
+    console.log(colorToHex(cell.css("background-color")));
+    console.log(color);
     cell.toggleClass("painted");
 });
 $('.clear').click(function(){
     $('tr').find('*').css("background-color", 'white');
 });
+
+
+function colorToHex(color) {
+    if (color.substr(0, 1) === '#') {
+        return color;
+    }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
+    
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
+    
+    var rgb = blue | (green << 8) | (red << 16);
+    return digits[1] + '#' + rgb.toString(16);
+};
